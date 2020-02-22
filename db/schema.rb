3d708c8c73bf56e76a9d1f4c80ae614c8a5ad107
taiga-tsuzuki <60640899+taiga-tsuzuki@users.noreply.github.com>
2020-02-22 17:17:05 +0900

    add_devise_user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_041112) do
+ActiveRecord::Schema.define(version: 2020_02_22_050503) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_02_22_041112) do
     t.text "video"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
